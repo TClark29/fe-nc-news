a6007382a3d81a32be7328d1d699e30fc4ca4e1f
TClark29 <tomclark@DESKTOP-ILEHEEH>
2024-03-11 15:21:33 +0000

    task 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
 import './App.css'
 import ArticlesPage from './Components/articles-page'
+import UserBar from './Components/user-bar'
+import ArticleSingle from './Components/articles-single'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   
 
   return (
     <>
-    <ArticlesPage></ArticlesPage>
-   
+    <UserBar></UserBar>
+      <Routes>
+        <Route path='/' element={<ArticlesPage></ArticlesPage>}/>
+        <Route path='/article/:id' element={<ArticleSingle></ArticleSingle>}/>
+      </Routes>
     </>
   )
 }
---
+++ src/Components/articles-single.jsx
@@ -0,0 +1,8 @@
+function ArticleSingle(){
+
+    return(
+        <h1>article</h1>
+    )
+}
+
+export default ArticleSingle
